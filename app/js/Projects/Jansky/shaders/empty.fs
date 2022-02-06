d693d652012d7632cdb0bfded6b56a1bd9b123fb
santi <santi-grau@users.noreply.github.com>
2022-02-06 10:22:49 -0800

    just.do.it.

--- app/js/Projects/Jansky/shaders/empty.fs
+++
@@ -1,7 +0,0 @@
-uniform sampler2D tDiffuse;
-varying vec2 vUv;
-
-void main(){
-	vec4 col = texture2D( tDiffuse, vUv );
-	gl_FragColor = col;
-}
--- app/js/Projects/Mugshots/shaders/mugshotsVs.glsl
+++
@@ -1,7 +0,0 @@
-varying vec2 vUv;
-uniform vec2 pos;
-
-void main() {
-	vUv = uv;
-	gl_Position = projectionMatrix * modelViewMatrix * vec4(position,1.0);
-}
